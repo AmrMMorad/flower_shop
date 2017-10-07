14811ccdc2f2e69cda579e13b5ff9b259de389a1
AmrMMorad <amr.morad@bibalex.org>
2017-10-07 11:15:06 +0200

    flower class created

---
+++ app/flower.rb
@@ -0,0 +1,17 @@
+class Flower
+  attr_reader :name, :code, :bundles
+
+  def initialize(name:, code:, bundles:)
+    validate_input name, code
+    @name = name
+    @code = code
+    @bundles = bundles
+  end
+  
+  private
+  
+  def validate_input(name, code)
+    raise ArgumentError, "invalid flower parameters" if name.blank? && code.blank?
+  end
+  
+end
---
+++ spec/app/flower_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe Flower do
+  
+  context '#new' do
+    
+    context 'with valid input values' do
+      let(:bundles) do
+        [
+          instance_double(Bundle),
+          instance_double(Bundle),
+          instance_double(Bundle)
+        ]
+      end
+      let(:flower) { Flower.new(name: 'Lilies', code: 'R12', bundles: bundles) }
+  
+      it 'has a name' do
+        expect(flower.name).to eq 'Lilies'
+      end
+
+      it 'has a code' do
+        expect(flower.code).to eq 'R12'
+      end
+
+      it 'has bundles' do
+        expect(flower.bundles).to eq bundles
+      end
+    end
+    
+    context 'with invalid input values' do
+      let(:bundles) do
+        [
+          instance_double(Bundle),
+          instance_double(Bundle)
+        ]
+      end
+      it 'raises an error when entering empty code' do
+        expect { Flower.new(name: '', code: 'R12', bundles: bundles).to raise_error(ArgumentError) }
+      end
+ 
+       it 'raises an error when entering empty name' do
+          expect { Flower.new(name: 'Lilies', code: '', bundles: bundles).to raise_error(ArgumentError) }
+        end
+    end
+    
+  end
+end
